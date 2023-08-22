4465dcdfccccfeae2291bf9abd3d09c497adf1e6
Alex Kollmeyer <akollmeyer123@gmail.com>
2023-08-22 13:56:38 -0500

    Initialization of project, ERD added, inital setup complete.

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Products_ReviewsAPI.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+    }
+}
