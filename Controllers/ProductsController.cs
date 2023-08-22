939641b53d4e62842af9ffb7399a8c5988389ae4
Alex Kollmeyer <akollmeyer123@gmail.com>
2023-08-22 17:05:22 -0500

    Fixed Application DbContext, created controllers and finished the productcontrollers Post method.

---
+++ Controllers/ProductsController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Products_ReviewsAPI.Data;
+using Products_ReviewsAPI.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Products_ReviewsAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ProductsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/<ProductsController>
+        [HttpGet]
+        public IEnumerable<string> GetAll()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<ProductsController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<ProductsController>
+        [HttpPost]
+        public IActionResult Post([FromBody] Product product)
+        {
+            _context.Products.Add(product);
+            _context.SaveChanges();
+            return Ok(product);
+        }
+
+        // PUT api/<ProductsController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<ProductsController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -5,6 +5,8 @@ namespace Products_ReviewsAPI.Data
 {
     public class ApplicationDbContext : DbContext
     {
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Review> Reviews { get; set; }
         public ApplicationDbContext(DbContextOptions options) : base(options)
         {
 
