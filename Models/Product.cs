aec41f13904d079218bad3237532f516d6fa76e7
Alex Kollmeyer <akollmeyer123@gmail.com>
2023-08-22 16:03:04 -0500

    Models created, fixed connection string, inital migration completed, database created

---
+++ Models/Product.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Products_ReviewsAPI.Models
+{
+    public class Product
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public ICollection<Review> Reviews { get; set; } //Nav property
+
+    }
+}
---
+++ Models/Review.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Products_ReviewsAPI.Models
+{
+    public class Review
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Text { get; set; }
+        public int Rating { get; set; }
+
+        [ForeignKey ("Product")]
+        public int ProductId { get; set; }
+        public Product Product { get; set; } //Nav property
+    }
+}
